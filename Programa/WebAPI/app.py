36b37078ec6cc612fc16eda4ce26494778b4d89c
Josue Guevara <99698293+PoncheDeFrutas@users.noreply.github.com>
2023-10-18 17:15:52 -0600

    Ingreso de base de datos y cambios varios
    Configuración inicial de la base de datos.
    Creación del apartado de solicitudes (request) con la capacidad de crear, configurar y recuperar datos de la base de datos.
    Implementación de la funcionalidad en files_request para manejar solicitudes de archivos, permitiendo almacenar mensajes y configuraciones.
    Configuración de Flask en app.py para admitir métodos GET y POST requeridos.
    Eliminación de clases previamente creadas, ahora los datos se almacenan automáticamente en la base de datos según su categoría."

--- Programa/WebAPI/app.py
+++ Programa/WebAPI/app.py
@@ -1,15 +1,106 @@
-from flask import Flask
-
-dictionary = None
-message_list = []
+from flask_cors import CORS
+from datetime import datetime
+from flask import Flask, jsonify, request
+from requests_.database_requests import initialize_db, clear_db, get_elements_between_dates, get_hashtags_count_by_date
+from requests_.database_requests import get_users_count_by_date, get_feelins_count_by_date
+from requests_.files_requests import process_xml_files, extract_messages, save_messages, extract_feelings
 
 app = Flask(__name__)
+CORS(app)
+app.config['DATABASE'] = 'database.db'
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def init():
+    return "nice"
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/grabarMensajes', methods=['POST'])
+def record_messages():
+    try:
+        response_data = process_xml_files()
+        messages = extract_messages(response_data)
+        response = save_messages(messages)
+        return response
+    except Exception as e:
+        return jsonify({'error': f'Error al procesar los archivos XML: {str(e)}'}), 500
+
+
+@app.route('/grabarConfiguracion', methods=['POST'])
+def record_config():
+    try:
+        response_data = process_xml_files()
+        response = extract_feelings(response_data)
+        return response
+    except Exception as e:
+        return jsonify({'error': f'Error al procesar los archivos XML: {str(e)}'}), 500
+
+
+@app.route('/limpiarDatos', methods=['POST'])
+def clear_database():
+    clear_db()
+    return 'Base de datos limpia'
+
+
+@app.route('/devolerHashtags', methods=['POST'])
+def post_return_hashtags():
+    pass
+
+
+@app.route('/devolerHashtags', methods=['GET'])
+def get_return_hashtags():
+    date_one = datetime.strptime(request.args.get('date_1'), "%d/%m/%Y")
+    date_two = datetime.strptime(request.args.get('date_2'), "%d/%m/%Y")
+    if date_one > date_two:
+        print(f"{date_one} es posterior a {date_two}")
+    elif date_one < date_two:
+        response = get_elements_between_dates(date_one, date_two)
+        return get_hashtags_count_by_date(response)
+    else:
+        print(f"{date_one} es igual a {date_two}")
+
+
+@app.route('/devolerMenciones', methods=['POST'])
+def post_return_mencions():
+    pass
+
+
+@app.route('/devolerMenciones', methods=['GET'])
+def get_return_mencions():
+    date_one = datetime.strptime(request.args.get('date_1'), "%d/%m/%Y")
+    date_two = datetime.strptime(request.args.get('date_2'), "%d/%m/%Y")
+    if date_one > date_two:
+        print(f"{date_one} es posterior a {date_two}")
+    elif date_one < date_two:
+        response = get_elements_between_dates(date_one, date_two)
+        return get_users_count_by_date(response)
+    else:
+        print(f"{date_one} es igual a {date_two}")
+
+
+@app.route('/devolverSentimientos', methods=['POST'])
+def post_return_feelings():
+    pass
+
+
+@app.route('/devolverSentimientos', methods=['GET'])
+def get_return_feelings():
+    date_one = datetime.strptime(request.args.get('date_1'), "%d/%m/%Y")
+    date_two = datetime.strptime(request.args.get('date_2'), "%d/%m/%Y")
+    if date_one > date_two:
+        print(f"{date_one} es posterior a {date_two}")
+    elif date_one < date_two:
+        response = get_elements_between_dates(date_one, date_two)
+        return get_feelins_count_by_date(response)
+    else:
+        print(f"{date_one} es igual a {date_two}")
+
+
+@app.route('/initialize_db', methods=['POST'])
+def initialize_database():
+    initialize_db()
+    return 'Base de datos inicializada'
+
+
+if __name__ == '__main':
+    app.run(debug=True, port=3050)
